24a5c12aa9f508afc8a3eee791208029d924df43
Andre Luiz <alandrade.andre@gmail.com>
2021-12-01 00:49:13 -0300

    Backend aplicação PI 202102 Fatec

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+module.exports = {
+  host: "localhost",
+  dialect: "mysql",
+  username: "root",
+  password: "root",
+  database: "piwater",
+  define: {
+      timestamps: true,
+      underscored: false,
+  },
+};
+
+
+
